0227683740a795f626f4b72ca9963db8fba219a5
rjnmattos <rjnmattos@gmail.com>
2019-01-29 19:39:40 -0300

    Error message

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -39,7 +39,7 @@ export class CategoryService {
   update(category: Category): Observable<Category> {
     const url = `${this.apiPath}/${category.id}`;
 
-    return this.http.put(url, category).pipe(
+    return this.http.put("sdad", category).pipe(
       map(() => category),
       catchError(this.handleError)
     );
