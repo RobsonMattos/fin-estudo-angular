b79ec2164a4d686e0e957e63140cd455a2ad91d5
RobsonMattos <rjnmattos@gmail.com>
2019-03-05 16:46:30 -0300

    alteração category e entry list

--- src/app/pages/entries/entries-list/entries-list.component.ts
+++ src/app/pages/entries/entries-list/entries-list.component.ts
@@ -1,33 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Entry } from '../shared/entry.model';
 import { EntryService } from '../shared/entry.service';
+import { BaseResourceListComponent } from 'src/app/shared/components/base-resource-list/base-resource-list.component';
 
 @Component({
   selector: 'app-entries-list',
   templateUrl: './entries-list.component.html',
   styleUrls: ['./entries-list.component.css']
 })
-export class EntriesListComponent implements OnInit {
-  entries: Entry[] = [];
-
-  constructor(private entryService: EntryService) { }
-
-  ngOnInit() {
-    this.entryService.getAll().subscribe(
-      entries => this.entries = entries.sort((a,b) => b.id - a.id),
-      error => console.log(error)
-    );
-  }
-
-  deleteEntry(entry) {
-    const mustDelete = confirm('Deseja realmente excluir esse item?');
-
-    if (mustDelete) {
-      this.entryService.delete(entry.id).subscribe(
-        () => this.entries = this.entries.filter(element => element !== entry),
-        () => alert('erro ao tentar excluir')
-      );
-    }
+export class EntriesListComponent extends BaseResourceListComponent<Entry> {
+  
+  constructor(private entryService: EntryService) { 
+    super(entryService);
   }
 
 }
