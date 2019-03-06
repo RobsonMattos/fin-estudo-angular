4cd3a5eff23a12db0ef17ff37d3a6de2b18080d4
RobsonMattos <rjnmattos@gmail.com>
2019-03-06 17:16:17 -0300

    reports

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, Injector } from '@angular/core';
 
-import { CategoryService } from "../../categories/shared/category.service";
+import { Observable } from "rxjs";
+import { flatMap, catchError, map } from "rxjs/operators";
 
+import { BaseResourceService } from "../../../shared/services/base-resource.service";
+import { CategoryService } from "../../categories/shared/category.service";
 import { Entry } from "./entry.model";
-import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
-import { Observable } from 'rxjs';
-import { flatMap, catchError } from 'rxjs/operators';
+
+import * as moment from "moment"
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +15,7 @@ import { flatMap, catchError } from 'rxjs/operators';
 export class EntryService extends BaseResourceService<Entry> {
 
   constructor(protected injector: Injector, private categoryService: CategoryService) { 
-    super('api/entries', injector, Entry.fromJson);
+    super("api/entries", injector, Entry.fromJson);
   }
 
   create(entry: Entry): Observable<Entry> {
@@ -21,7 +23,13 @@ export class EntryService extends BaseResourceService<Entry> {
   }
 
   update(entry: Entry): Observable<Entry> {
-    return this.setCategoryAndSendToServer(entry, super.update.bind(this));
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this))
+  }
+
+  getByMonthAndYear(month: number, year: number): Observable<Entry[]> {
+    return this.getAll().pipe(
+      map(entries => this.filterByMonthAndYear(entries, month, year))
+    )
   }
 
   private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry>{
@@ -32,6 +40,16 @@ export class EntryService extends BaseResourceService<Entry> {
       }),
       catchError(this.handleError)
     );
+  }
+
+  private filterByMonthAndYear(entries: Entry[], month: number, year: number) {
+    return entries.filter(entry => {
+      const entryDate = moment(entry.date, "DD/MM/YYYY");
+      const monthMatches = entryDate.month() + 1 == month;
+      const yearMatches = entryDate.year() == year;
+
+      if(monthMatches && yearMatches) return entry;
+    })
   }
 
 }
